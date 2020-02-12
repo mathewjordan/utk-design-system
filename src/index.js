5ef7731ef3930ceb31633b90321dcdc121c705e7
Mat Jordan <mat@utk.edu>
2020-02-12 08:21:40 -0500

    Port react based system.

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import System from './System';
-import './scss/styles.scss';
-
-if (document.getElementById('utk-design-system')) {
-    ReactDOM.render(<System />, document.getElementById('utk-design-system'));
-}
