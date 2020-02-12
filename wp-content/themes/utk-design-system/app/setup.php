aa67dd0599975ee46ba2e108c8b3aaae22cbd89e
Mat Jordan <mat@utk.edu>
2020-02-12 10:36:37 -0500

    Extend gutenberg button.

--- wp-content/themes/utk-design-system/app/setup.php
+++ wp-content/themes/utk-design-system/app/setup.php
@@ -11,13 +11,17 @@ use Roots\Sage\Template\BladeProvider;
  * Theme assets
  */
 add_action('wp_enqueue_scripts', function () {
-    wp_enqueue_style('sage/main.css', asset_path('styles/main.css'), false, null);
-    wp_enqueue_script('sage/main.js', asset_path('scripts/main.js'), ['jquery'], null, true);
+    wp_enqueue_style('utk/main.css', asset_path('styles/main.css'), false, null);
+    wp_enqueue_script('utk//main.js', asset_path('scripts/main.js'), ['jquery'], null, true);
 
     if (is_single() && comments_open() && get_option('thread_comments')) {
         wp_enqueue_script('comment-reply');
     }
 }, 100);
+
+add_action('admin_enqueue_scripts', function () {
+    wp_enqueue_style('utk//admin.css', asset_path('styles/admin.css'), false, null);
+});
 
 /**
  * Theme setup
