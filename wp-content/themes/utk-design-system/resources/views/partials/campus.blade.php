3485f0d42d5184b1554b774ddb70dacf2b5467c9
Mat Jordan <mat@utk.edu>
2020-02-14 09:01:18 -0500

    Add jumbotron styling.

--- wp-content/themes/utk-design-system/resources/views/partials/campus.blade.php
+++ wp-content/themes/utk-design-system/resources/views/partials/campus.blade.php
@@ -1,12 +1,15 @@
 <div class="campus">
   <div class="container">
     <div class="campus-inner">
-      <a class="utk-campus-back" href="">
-      </a>
-      <a class="utk-campus-nav" href="">
-        Campus
-        <span class="icon-down-open"></span>
-      </a>
+      <div class="campus-inner-menu">
+        <a class="utk-campus-nav" href="">
+          Campus
+          <span class="icon-down-open"></span>
+        </a>
+        <a class="utk-campus-accessiblity" href="">
+          Accessibility
+        </a>
+      </div>
       <form class="utk-campus-search">
         <input placeholder="search utk.edu" />
         <button>Search</button>
---
+++ wp-content/themes/utk-design-system/resources/views/partials/jumbotron.blade.php
@@ -0,0 +1,16 @@
+<section class="jumbotron">
+  <div class="container">
+    <div class="jumbotron-content">
+      <h2>Vulputate ex Tincidun</h2>
+      <p>In ut ullamcorper magna. Aliquam aliquam pulvinar urna.</p>
+      <a class="btn btn-muted btn-outline">
+        Magna Quis
+        <span class="icon-right-big"></span>
+      </a>
+    </div>
+  </div>
+  <div class="jumbotron-backdrop">
+    <img src="@asset('images/jumbotron-placeholder.jpg')"
+         alt="torchbearer among the evening sky" />
+  </div>
+</section>
